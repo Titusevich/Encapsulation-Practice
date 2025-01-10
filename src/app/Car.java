6f2c705af670fef9108587f4c4fae8996ffbeef1
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-10 15:40:53 +0200

    Add new private methods in class Car

--- src/app/Car.java
+++ src/app/Car.java
@@ -9,4 +9,16 @@ public class Car {
         System.out.println("------------------------------------------------" +
                 "\n" + "Car is successfully activated.");
     }
+
+    private void startElectricity() {
+        System.out.println("Electric system of car started successfully.");
+    }
+
+    private void startCommand() {
+        System.out.println("   Command system of car is initialized.");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("   Fuel system of car is activated successfully.");
+    }
 }
