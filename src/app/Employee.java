6c1247bcae31d1c9b773ba18099b06d4fc2defcb
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-10 15:15:15 +0200

    Add new class Employee

---
+++ src/app/Employee.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Employee {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String position;
+    private String email;
+    private String phone;
+    private int age;
+}
