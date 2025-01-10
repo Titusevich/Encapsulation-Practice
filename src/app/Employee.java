cbd342d0b6a658cadc619f247899071cfd7ff32b
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-10 15:19:48 +0200

    Add constructor of class Employee

--- src/app/Employee.java
+++ src/app/Employee.java
@@ -9,4 +9,15 @@ public class Employee {
     private String email;
     private String phone;
     private int age;
+
+    public Employee(String firstName, String middleName, String lastName,
+                    String position, String email, String phone, int age) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
 }
